c9232e54b675d0cb6385601cb945ec16719c20a2
jnhenkel <jeffnhenkel@gmail.com>
2022-03-31 12:34:15 -0700

    test

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const app = express();
 const port = process.env.PORT || 4002;
 
 //middlewares
+app.use(cors());
 app.use(express.json());
 
 //default root
@@ -17,14 +18,14 @@ app.get('/', (req, res) => {
 });
 
 //1
-app.post('/register', (request, response) => {
-    let username = request.body.username;
-    let email = request.body.email;
-    let password = request.body.password; /* store will handle encryption */
+app.post('/register', (req, res) => {
+    let username = req.body.username;
+    let email = req.body.email;
+    let password = req.body.password; /* store will handle encryption */
     if (store.addCustomer(username, email, password)) {
-    response.status(200).json({done: true, message: 'A customer has been added successfully'});
+    res.status(200).json({done: true, message: 'A customer has been added successfully'});
     } else {
-        response.status(403).json({done: false, message: 'A customer already exists with that email'});
+        res.status(403).json({done: false, message: 'A customer already exists with that email'});
     }
 });
 
