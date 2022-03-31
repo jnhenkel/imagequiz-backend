26bb64cfba94ce4eb7a3990b013fc3bed0652dfa
jnhenkel <jeffnhenkel@gmail.com>
2022-03-31 12:19:50 -0700

    testing flowers

--- index.js
+++ index.js
@@ -44,7 +44,7 @@ app.post('/login', (req, res) => {
 app.get('/flowers', (req, res) => {
     flowerNames = [];
     
-    res.status(200).json({done: true, result: flowers.name, message: 'These are the flowers from flowers.js'})
+    res.status(200).json({done: true, result: flowers, message: 'These are the flowers from flowers.js'})
 });
 
 app.listen(port, () => {
