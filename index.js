184b4b6ca064e26c388b4d5f7cdb7d87f009d605
jnhenkel <jeffnhenkel@gmail.com>
2022-03-31 12:50:17 -0700

    update register

--- index.js
+++ index.js
@@ -23,7 +23,8 @@ app.post('/register', (req, res) => {
     let username = req.body.username;
     let email = req.body.email;
     let password = req.body.password; /* store will handle encryption */
-    if (store.addCustomer(username, email, password)) {
+    let entry = store.addCustomer(username, email, password);
+    if (entry) {
         res.status(200).json({ done: true, message: 'A customer has been added successfully' });
     } else {
         res.status(403).json({ done: false, message: 'A customer already exists with that email' });
