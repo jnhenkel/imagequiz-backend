66d624f4254c654c1ad051450bdcdfbf2df3db6f
jnhenkel <jeffnhenkel@gmail.com>
2022-03-30 18:40:47 -0700

    #1 finished

--- data/store.js
+++ data/store.js
@@ -6,11 +6,19 @@ let store = {
         const hashed = bcrypt.hashSync(password, 10);//hash pw before sending to customers
         let newID;
         if (customers.length) {
+            //first check if username exists
+            for (let i=0; i<customers.length; i++) {
+                if (customers[i].name == username) {
+                    return false;
+                }
+            }
+            //set new id to be last id in db plus 1
             newID = customers[customers.length - 1].id + 1;
         } else {
             newID = 1;
         }
         customers.push({ id: newID, name: username, email: email, password: hashed });
+        return true;
     }
 }
 
--- index.js
+++ index.js
@@ -14,8 +14,11 @@ app.post('/register', (req, res) => {
     let username = req.body.username;
     let email = req.body.email;
     let password = req.body.password; /* store will handle encryption */
-    store.addCustomer(username, email, password);
+    if (store.addCustomer(username, email, password)) {
     res.status(200).json({done: true, message: 'A customer has been added successfully'});
+    } else {
+        res.status(403).json({done: false, message: 'A customer already exists with that email'});
+    }
 })
 
 app.listen(port, () => {
