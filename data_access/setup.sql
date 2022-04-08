bb3f0fa6889a1c029f6e79192b2d549d9f8302a4
jnhenkel <jeffnhenkel@gmail.com>
2022-04-08 16:49:47 -0700

    sql setupd script

---
+++ data_access/setup.sql
@@ -0,0 +1,53 @@
+create schema if not exists imagequiz;
+
+drop table if exists imagequiz.score;
+drop table if exists imagequiz.customer;
+drop table if exists imagequiz.quiz_question; 
+drop table if exists imagequiz.question;
+drop table if exists imagequiz.quiz; 
+drop table if exists imagequiz.category; 
+drop table if exists imagequiz.flowers; 
+
+create table imagequiz.customer (
+	id bigserial primary key,
+	name varchar(255) not null,
+	email varchar(255) not null unique,
+	password varchar(255) not null
+);
+
+create table imagequiz.question (
+	id bigserial primary key,
+	picture varchar(255) not null,
+	choices varchar(255) not null,
+	answer varchar(255) not null
+);
+
+create table imagequiz.category (
+	id bigserial primary key,
+	name varchar(255) not null
+);
+
+create table imagequiz.quiz (
+	id bigserial primary key,
+	name varchar(255) not null,
+	category_id int references imagequiz.category(id)
+);
+
+create table imagequiz.quiz_question (
+	quiz_id int references imagequiz.quiz(id),
+	question_id int references imagequiz.question(id)
+)
+
+create table imagequiz.flower (
+	id bigserial primary key,
+	name varchar(255) not null,
+	picture varchar(255) not null
+);
+
+create table imagequiz.score (
+	id bigserial primary key,
+	customer_id int references imagequiz.customer(id),
+	quiz_id int references imagequiz.quiz(id),
+	date timestamp not null,
+	score float8 not null
+);
