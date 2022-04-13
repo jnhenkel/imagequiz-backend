336169acf8eb27c57733f7281311879d8711a947
jnhenkel <jeffnhenkel@gmail.com>
2022-04-13 10:26:43 -0700

    login connected to heroku db

--- data_access/store.js
+++ data_access/store.js
@@ -17,6 +17,22 @@ let store = {
     addCustomer: (name, email, password) => {
         const hash = bcrypt.hashSync(password, 10);
         return pool.query('INSERT INTO imagequiz.customer (name, email, password) VALUES ($1, $2, $3)', [name, email, hash]);
+    },
+
+    login: (email, password) => {
+        return pool.query('SELECT name, email, password FROM imagequiz.customer WHERE email = $1', [email])
+        .then(x => {
+            if (x.rows.length ==1) {
+                let valid = bcrypt.compareSync(password, x.rows[0].password);
+                if (valid) {
+                    return {valid: true};
+                } else {
+                    return {valid: false, message: 'Credentials are not valid.'};
+                }
+            } else {
+                return {valid: false, message: 'Email not found.'};
+            }
+        });
     }
 }
 
